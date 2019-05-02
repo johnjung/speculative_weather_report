cc461b769795526dce9c51210f4a9fffa5551840
John Jung <jej@moss.lib.uchicago.edu>
2019-05-02 16:54:23 -0500

    work in progress, split code into three classes

--- index.py
+++ index.py
@@ -14,6 +14,45 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
+
+class Forecast:
+    def __init__(self):
+        self.current_weather = Weather()
+        self.news = News()
+
+    def daily(self):
+        forecast = []
+        w = self
+        for i in range(7):
+            w = w.get_next_day_weather()
+            forecast.append(w.asdict())
+        return forecast
+
+    def hourly(self):
+        forecast = []
+        w = self
+        for i in range(24):
+            w = w.get_next_day_weather()
+            forecast.append(w.asdict())
+        return forecast
+
+    def future_years_with_same_weekday(self):
+        years = []
+        for y in range(1, 100):
+            future_d = self.datetime + datetime.timedelta(year=y)
+            if future_d.weekday() == self.datetime.weekday():
+                years.append(y)
+        return years
+
+    def asdict(self):
+        return {
+            'current_weather': self.current_weather.asdict(),
+            'daily_forecast':  self.daily_forecast(),
+            'hourly_forecast': self.hourly_forecast(),
+            'news':            self.news
+        }
+
+
 class Weather:
     def __init__(self, hdt, cdt, fdt):
         '''Constructor
@@ -39,7 +78,16 @@ class Weather:
                 self.historical_data.append(row)
         self.i = self.get_closest_past_index(historical_datetime)
 
-    def get_carbon_count(self, temperature_increase):
+    @property
+    def as_of(self):
+        '''Get the most recent reading time from historical data.
+
+        :returns a datetime string.
+        '''
+        return self.get_historical('DATE')
+
+    @property
+    def carbon_count(self, temperature_increase):
         '''Get an estimated carbon count in ppm for a temperature increase
         given in F, compared to current levels:
         http://dels.nas.edu/resources/static-assets/materials-based-on-reports/booklets/warming_world_final.pdf
@@ -47,14 +95,16 @@ class Weather:
         carbon_counts = (410, 480, 550, 630, 700, 800, 900, 1000, 1200, 1400)
         return carbon_counts[temperature_increase]
 
-    def get_dew_point(self):
+    @property
+    def dew_point(self):
         '''Get the dew point.
 
         :returns the dew point temperature as an integer in F. 
         '''
         return int(self.get_historical('HourlyDewPointTemperature'))
 
-    def get_heat_index(self):
+    @property
+    def heat_index(self):
         '''Calculate the heat index: see
         https://en.wikipedia.org/wiki/Heat_index.
 
@@ -80,49 +130,16 @@ class Weather:
             )
         )
 
-    def get_present_weather_type(self):
-        '''Get a description of the current weather. 
-
-        :returns a string.
-        '''
-        weather_strings = {
-            'FG':   'fog',
-            'TS':   'thunder',
-            'PL':   'sleet',
-            'GR':   'hail',
-            'GL':   'ice sheeting',
-            'DU':   'dust',
-            'HZ':   'haze',
-            'BLSN': 'drifing snow',
-            'FC':   'funnel cloud',
-            'WIND': 'high winds',
-            'BLPY': 'blowing spray',
-            'BR':   'mist',
-            'DZ':   'drizzle',
-            'FZDZ': 'freezing drizzle',
-            'RA':   'rain',
-            'FZRA': 'freezing rain',
-            'SN':   'snow',
-            'UP':   'precipitation',
-            'MIFG': 'ground fog',
-            'FZFG': 'freezing fog'
-        }
-        types = set()
-        present_weather = self.get_historical('HourlyPresentWeatherType')
-        for p in present_weather.split('|'):
-            m = re.search('[A-Z]+', p)
-            if m:
-                types.add(weather_strings[m.group(0)])
-        return ', '.join(list(types))
-
-    def get_relative_humidity(self):
+    @property
+    def relative_humidity(self):
         '''Get the relative humidity.
 
         :returns the relative humidity as an integer from 0 to 100. 
         '''
         return int(self.get_historical('HourlyRelativeHumidity'))
 
-    def get_sky_conditions(self):
+    @property
+    def sky_conditions(self):
         '''Get sky conditions. These are recorded in the data in a string like:
 
         FEW:02 70 SCT:04 200 BKN:07 250
@@ -160,24 +177,68 @@ class Weather:
         except AttributeError:
             return ''
 
-    def get_temperature(self):
+    @property
+    def temperature(self):
         '''Get the dry bulb temperature ("the temperature")
 
         :returns the temperature as an integer in F.
         '''
         return int(self.get_historical('HourlyDryBulbTemperature'))
 
-    def get_reading_time(self):
-        '''Get the most recent reading time from historical data.
+    @property
+    def temperature_min(self):
+        return self._temperature_summary('min')
 
-        :returns a datetime string.
-        '''
-        return self.get_historical('DATE')
+    @property
+    def temperature_mean(self):
+        return self._temperature_summary('mean')
 
-    def get_visibility(self):
+    @property
+    def temperature_max(self):
+        return self._temperature_summary('max')
+
+    @property
+    def visibility(self):
         return int(float(self.get_historical('HourlyVisibility')))
 
-    def get_wind_direction_and_speed(self):
+    @property
+    def weather_type(self):
+        '''Get a description of the current weather. 
+
+        :returns a string.
+        '''
+        weather_strings = {
+            'FG':   'fog',
+            'TS':   'thunder',
+            'PL':   'sleet',
+            'GR':   'hail',
+            'GL':   'ice sheeting',
+            'DU':   'dust',
+            'HZ':   'haze',
+            'BLSN': 'drifing snow',
+            'FC':   'funnel cloud',
+            'WIND': 'high winds',
+            'BLPY': 'blowing spray',
+            'BR':   'mist',
+            'DZ':   'drizzle',
+            'FZDZ': 'freezing drizzle',
+            'RA':   'rain',
+            'FZRA': 'freezing rain',
+            'SN':   'snow',
+            'UP':   'precipitation',
+            'MIFG': 'ground fog',
+            'FZFG': 'freezing fog'
+        }
+        types = set()
+        present_weather = self.get_historical('HourlyPresentWeatherType')
+        for p in present_weather.split('|'):
+            m = re.search('[A-Z]+', p)
+            if m:
+                types.add(weather_strings[m.group(0)])
+        return ', '.join(list(types))
+
+    @property
+    def wind_direction_and_speed(self):
         d = int(self.get_historical('HourlyWindDirection'))
         if d == 0:
             return 'still'
@@ -190,33 +251,8 @@ class Weather:
         s = self.get_historical('HourlyWindSpeed')
         return '{}mph {}'.format(s, direction)
 
-    def get_temperature_summary(self, summary_type):
-        '''Get the high temperature for the day.
-
-        :param str summary_type: one of 'min', 'max', 'mean'
-
-        :returns the high temperature as an integer in F.
-        '''
-        temperatures = map(
-            int,
-            filter(
-                bool,
-                self.get_historical_range(
-                    'HourlyDryBulbTemperature',
-                    *self.get_daily_timestring_range()
-                )
-            )
-        )
-        if summary_type == 'min':
-            return min(temperatures)
-        elif summary_type == 'max':
-            return max(temperatures)
-        elif summary_type == 'mean':
-            return int(statistics.mean(temperatures))
-        else:
-            raise ValueError
-
-    def get_beginning_of_day_weather(self):
+    @property
+    def start_of_day(self):
 	'''Factory for a new Weather() object with its datetime set at the
 	beginning of the current object's day. (i.e., midnight of the current
         day.)
@@ -232,7 +268,8 @@ class Weather:
         	datetime.datetime(self.fdt.year, self.fdt.month, self.fdt.day)
             )
 
-    def get_next_day_weather(self):
+    @property
+    def next_day(self):
 	'''Factory for a new Weather() object with its datetime set at the
 	beginning of tomorrow (i.e. midnight), relative to the current
         instance. 
@@ -257,7 +294,8 @@ class Weather:
             ) + datetime.timedelta(days=1)
         )
 
-    def get_previous_day_weather(self):
+    @property
+    def previous_day(self):
 	'''Factory for a new Weather() object. If the current instance's
 	datetime is at the beginning of the day, this method returns an object
 	for the previous calendar day. Otherwise it returns an object for the
@@ -286,7 +324,8 @@ class Weather:
         else:
             return self.get_beginning_of_day_weather()
 
-    def get_sunrise_weather(self):
+    @property
+    def next_sunrise(self):
         '''Get weather for the first future sunrise from this point.
         '''
         sun = self.city.sun(date=self.cdt)
@@ -298,7 +337,8 @@ class Weather:
         raise NotImplementedError
         return Weather()
 
-    def get_sunset_weather(self):
+    @property
+    def next_sunset(self):
         '''Get weather for the first future sunset from this point. 
         '''
         sun = self.city.sun(date=self.cdt)
@@ -310,7 +350,8 @@ class Weather:
         raise NotImplementedError
         return Weather()
 
-    def get_beginning_of_hour_weather(self):
+    @property
+    def top_of_the_hour(self):
 	'''Factory for a new Weather() object. If the current instance's
 	datetime is at the beginning of the hour, return the current object.
         Otherwise return a new instance whose datetime is set to 00:00.
@@ -340,8 +381,9 @@ class Weather:
                     self.fdt.hour
                 ),
             )
-     
-    def get_next_hour_weather(self):
+    
+    @property 
+    def next_hour(self):
 	'''Factory for a new Weather() object with it's datetime set to the
         next hour.
 
@@ -368,7 +410,8 @@ class Weather:
             ) + datetime.timedelta(hours=1),
         )
 
-    def get_previous_hour_weather(self):
+    @property
+    def previous_hour(self):
 	'''Factory for a new Weather() object with it's datetime set to the
         beginning of the current hour or the previous hour. 
 
@@ -398,33 +441,11 @@ class Weather:
         else:
             return self.get_beginning_of_hour_weather()
 
-    def get_daily_forecast(self):
-        forecast = []
-        w = self
-        for i in range(7):
-            w = w.get_next_day_weather()
-            forecast.append({
-                'ts':   w.get_daily_forecast_time(),
-                'low':  w.get_temperature_summary('min'),
-                'high': w.get_temperature_summary('max'),
-                'day':  w.get_daily_forecast_time()
-            })
-        return forecast
-
-    def get_hourly_forecast(self):
-        forecast = []
-        w = self
-        for i in range(24):
-            forecast.append({
-                'ts':   w.get_hourly_forecast_time(),
-                'temp': w.get_temperature(),
-                'time': w.get_hourly_forecast_time()
-            })
-        return forecast
-
-    def get_moon_phase(self):
+    @property
+    def moon_phase(self):
         raise NotImplementedError
 
+    @property
     def is_beginning_of_day(self):
         '''Check to see if the current time is 00:00:00.
 
@@ -436,7 +457,8 @@ class Weather:
             self.cdt.second
         ) == (0, 0, 0)
 
-    def is_beginning_of_hour(self):
+    @property
+    def is_top_of_the_hour(self):
 	'''Check to see if the current object's datetime is set to the
         beginning of the hour, e.g. 00:00. 
 
@@ -447,19 +469,23 @@ class Weather:
             self.cdt.second
         ) == (0, 0)
 
+    @ property
     def is_daytime(self):
         raise NotImplementedError
 
-    def get_hourly_forecast_time(self):
+    @property
+    def human_readable_hourly_forecast_time(self):
         raise NotImplementedError
 
-    def get_daily_forecast_time(self):
+    @property
+    def human_readable_daily_forecast_time(self):
         raise NotImplementedError
 
-    def get_current_weather_time(self):
+    @property
+    def human_readable_current_time(self):
         raise NotImplementedError
 
-    def get_closest_past_index(self, datetime=None):
+    def _get_closest_past_index(self, datetime=None):
         '''Find the closest past index represented in historical data for a
         given date/time string.
 
@@ -476,7 +502,7 @@ class Weather:
             i = i - 1
         return i
 
-    def get_historical(self, field):
+    def _get_historical(self, field):
         '''Get a single historical data point.
 
         :param str field: the field name.
@@ -491,7 +517,7 @@ class Weather:
             i = i - 1
         return ''
 
-    def get_historical_range(self, field, datetime_lo, datetime_hi):
+    def _get_historical_range(self, field, datetime_lo, datetime_hi):
         '''Get a range of historical data points. 
 
         :param str field: the field name.
@@ -513,6 +539,51 @@ class Weather:
             r = r + 1
         return out
 
+    def _temperature_summary(self, summary_type):
+        '''Get the high temperature for the day.
+
+        :param str summary_type: one of 'min', 'max', 'mean'
+
+        :returns the high temperature as an integer in F.
+        '''
+        temperatures = map(
+            int,
+            filter(
+                bool,
+                self.get_historical_range(
+                    'HourlyDryBulbTemperature',
+                    *self.get_daily_timestring_range()
+                )
+            )
+        )
+        if summary_type == 'min':
+            return min(temperatures)
+        elif summary_type == 'max':
+            return max(temperatures)
+        elif summary_type == 'mean':
+            return int(statistics.mean(temperatures))
+        else:
+            raise ValueError
+
+    def asdict(self):
+        return {
+            'as_of':                    self.as_of,
+            'carbon_count':             self.carbon_count,
+            'dew_point':                self.dew_point,
+            'heat_index':               self.heat_index,
+            'relative_humidity':        self.relative_humidity,
+            'sky_conditions':           self.sky_conditions,
+            'temperature':              self.temperature,
+            'temperature_min':          self.temperature,
+            'temperature_mean':         self.temperature,
+            'temperature_max':          self.temperature,
+            'visibility':               self.visibility,
+            'weather_type':             self.weather_type,
+            'wind_direction_and_speed': self.wind_direction_and_speed
+        }
+
+
+class News:
     def get_advertisement(self):
         ads = (("Skirts, blouses and accessories. Up to 45% off. Shop Now.", "Noracora"),
                ("Your future's looking up with our new student loan. Competitive interest rates. Multiple repayment options. No origination fee. Get started now.", "Sallie Mae"),
@@ -538,14 +609,6 @@ class Weather:
                 ("Allergy: Spring Allergy Capitals: Which City Ranks the Worst",
                  "weather.com 2019/04/28 (verbatim)"))
         return [n[0] for n in random.sample(news, 3)]
-
-    def get_future_years_with_same_weekday(self):
-        years = []
-        for y in range(1, 100):
-            future_d = self.datetime + datetime.timedelta(year=y)
-            if future_d.weekday() == self.datetime.weekday():
-                years.append(y)
-        return years
 
 
 @app.route('/', methods=['GET'])
@@ -575,31 +638,8 @@ def index():
     )
     w = Weather(hdt, cdt, fdt)
 
-    future_datetime = datetime.datetime.strptime(
-        '{}-{:02}-{:02}T00:00:00'.format(
-            future_year,
-            now.month,
-            now.day
-        ),
-        '%Y-%m-%dT%H:%M:%S'
-    ).strftime('%A, %B, %-d %Y')
-
-
     moon_phases = ('New Moon', 'First Quarter', 'Full Moon', 'Last Quarter', 'New Moon')
     i = int(float(a.moon_phase(date=datetime.datetime.now())) / 7.0)
     "Moon phase: {}<br/>".format(moon_phases[i])
 
-    return render_template('weather.html', **{
-        'place':             'Chicago',
-        'weather_data_date': future_datetime,
-        'weather_data_time': datetime.datetime.strptime(
-                                 w.get_time(),
-                                 '%Y-%m-%dT%H:%M:%S'
-                             ).strftime('%-I:%M%p'),
-        'present_weather':   w.get_present_weather_type(),
-        'sky_conditions':    w.get_sky_conditions(),
-        'temperature':       w.get_temperature(),
-        'daily_forecast':    w.get_daily_forecast(),
-        'hourly_forecast':   w.get_hourly_forecast(),
-        'news':              ' '.join(w.get_news()) + ' ' + w.get_advertisement()
-    })
+    return render_template('weather.html', **w.asdict())
