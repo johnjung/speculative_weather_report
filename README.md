72f7a0fb9eb52745649bea9c5e596a8bc1137c04
John Jung <john@fieldrotation.com>
2019-05-05 15:33:27 -0500

    updated README.md

--- README.md
+++ README.md
@@ -1,20 +1,35 @@
 # Weather
 
-This is work in progress for a weather visualization. 
+This is work in progress for speculative design project about climate. It uses
+[NCEI Data Access for Land-Based
+Data](https://www.ncdc.noaa.gov/data-access/land-based-station-data/land-based-datasets)
+to produce a realistic looking weather forecast based on historical weather data.
 
-## Data
+```console
+$ python cli.py weather
 
-[NCEI Data Access for Land-Based Data](https://www.ncdc.noaa.gov/data-access/land-based-station-data/land-based-datasets)
+                    Sunday, May 5, 2069                     
 
-Some interesting fields:
+               -CURRENT WEATHER AS OF 2:53PM-               
 
-* HourlyDewPointTemperature
-* HourlyDryBulbTemperature
-* HourlyPrecipitation
-* HourlyRelativeHumidity
-* HourlySeaLevelPressure
-* HourlySkyConditions
-* HourlyStationPressure
-* HourlyVisibility
-* HourlyWindDirection
-* HourlyWindSpeed
+              clear sky, rain. Wind 10mph SSW.              
+
+        Current Temp: 93     
+                High: 94             Rel. Humidity: 25     
+                 Low: 49              Carbon Count: 410    
+
+                   -YOUR HOURLY FORECAST-                   
+
+  94   93   90   80   72   70   68   67   69   66   61   67 
+ 4PM  5PM  6PM  7PM  8PM  9PM 10PM 11PM 12AM  1AM  2AM  3AM 
+
+  67   66   65   67   70   74   77   84   87   89   91   92 
+ 4AM  5AM  6AM  7AM  8AM  9AM 10AM 11AM 12PM  1PM  2PM  3PM 
+
+                   -YOUR DAILY FORECAST-                    
+
+       low temperature:    61    72    68    63    73    73
+      mean temperature:    75    81    71    73    78    80
+      high temperature:    92    94    78    80    87    91
+                          Mon   Tue   Wed   Thu   Fri   Sat
+```
--- cli.py
+++ cli.py
@@ -1,94 +1,119 @@
+#!/usr/bin/env python
+'''Usage:
+    ./cli.py weather
+    ./cli.py load_data <csv_file>
+    ./cli.py get_field <field>
+'''
+
 import datetime
+import sqlite3
 import sys
 
+from docopt import docopt
 from weather import CurrentWeather, DailyWeather, Forecast, HourlyWeather, \
                     News, Sunrise, Sunset, Weather
 
-f = Forecast(datetime.datetime.now()).asdict()
-
-sys.stdout.write(
-    '\n{:^60}\n'.format(
-        '{}, {}'.format(
-            f['current_weather']['human_readable_datetime'],
-            f['current_weather']['simulation_year']
+def print_weather(f):
+    sys.stdout.write(
+        '\n{:^60}\n'.format(
+            '{}, {}'.format(
+                f['current_weather']['human_readable_datetime'],
+                f['current_weather']['simulation_year']
+            )
         )
     )
-)
-
-sys.stdout.write(
-    '\n{:^60}\n'.format(
-        '-CURRENT WEATHER AS OF {}-'.format(
-            f['current_weather']['as_of']
+    
+    sys.stdout.write(
+        '\n{:^60}\n'.format(
+            '-CURRENT WEATHER AS OF {}-'.format(
+                f['current_weather']['as_of']
+            )
         )
     )
-)
-
-sys.stdout.write(
-    '\n{:^60}\n\n'.format(
-        '{}, {}. Wind {}.'.format(
-            f['current_weather']['sky_conditions'],
-            f['current_weather']['weather_type'],
-            f['current_weather']['wind_direction_and_speed']
+    
+    sys.stdout.write(
+        '\n{:^60}\n\n'.format(
+            '{}, {}. Wind {}.'.format(
+                f['current_weather']['sky_conditions'],
+                f['current_weather']['weather_type'],
+                f['current_weather']['wind_direction_and_speed']
+            )
         )
     )
-)
-
-sys.stdout.write(
-    '{:>20}: {:<7}\n'.format(
-        'Current Temp',
-        f['current_weather']['temperature']
+    
+    sys.stdout.write(
+        '{:>20}: {:<7}\n'.format(
+            'Current Temp',
+            f['current_weather']['temperature']
+        )
     )
-)
-
-sys.stdout.write(
-    '{:>20}: {:<7} {:>20}: {:<7}\n'.format(
-        'High',
-        f['current_weather']['temperature_max'],
-        'Rel. Humidity',
-        f['current_weather']['relative_humidity']
+    
+    sys.stdout.write(
+        '{:>20}: {:<7} {:>20}: {:<7}\n'.format(
+            'High',
+            f['current_weather']['temperature_max'],
+            'Rel. Humidity',
+            f['current_weather']['relative_humidity']
+        )
     )
-)
-
-sys.stdout.write(
-    '{:>20}: {:<7} {:>20}: {:<7}\n'.format(
-        'Low',
-        f['current_weather']['temperature_min'],
-        'Carbon Count',
-        f['current_weather']['carbon_count']
+    
+    sys.stdout.write(
+        '{:>20}: {:<7} {:>20}: {:<7}\n'.format(
+            'Low',
+            f['current_weather']['temperature_min'],
+            'Carbon Count',
+            f['current_weather']['carbon_count']
+        )
     )
-)
+    
+    sys.stdout.write('\n{:^60}\n\n'.format('-YOUR HOURLY FORECAST-'))
+    
+    for h in f['hourly'][:12]:
+        sys.stdout.write('{:>4} '.format(h['temperature']))
+    sys.stdout.write('\n')
+    for h in f['hourly'][:12]:
+        sys.stdout.write('{:>4} '.format(h['human_readable_datetime']))
+    sys.stdout.write('\n\n')
+    
+    for h in f['hourly'][12:]:
+        sys.stdout.write('{:>4} '.format(h['temperature']))
+    sys.stdout.write('\n')
+    for h in f['hourly'][12:]:
+        sys.stdout.write('{:>4} '.format(h['human_readable_datetime']))
+    sys.stdout.write('\n')
+    
+    sys.stdout.write('\n{:^60}\n\n'.format('-YOUR DAILY FORECAST-'))
+    
+    sys.stdout.write('{:>23}'.format('low temperature:'))
+    for d in f['daily']:
+        sys.stdout.write('{:>6}'.format(d['temperature_min']))
+    sys.stdout.write('\n')
+    sys.stdout.write('{:>23}'.format('mean temperature:'))
+    for d in f['daily']:
+        sys.stdout.write('{:>6}'.format(d['temperature_mean']))
+    sys.stdout.write('\n')
+    sys.stdout.write('{:>23}'.format('high temperature:'))
+    for d in f['daily']:
+        sys.stdout.write('{:>6}'.format(d['temperature_max']))
+    sys.stdout.write('\n')
+    sys.stdout.write('{:>23}'.format(''))
+    for d in f['daily']:
+        sys.stdout.write('{:>6}'.format(d['human_readable_datetime']))
+    sys.stdout.write('\n\n')
 
-sys.stdout.write('\n{:^60}\n\n'.format('-YOUR HOURLY FORECAST-'))
+if __name__=='__main__':
+    arguments = docopt(__doc__)
 
-for h in f['hourly'][:12]:
-    sys.stdout.write('{:>4} '.format(h['temperature']))
-sys.stdout.write('\n')
-for h in f['hourly'][:12]:
-    sys.stdout.write('{:>4} '.format(h['human_readable_datetime']))
-sys.stdout.write('\n\n')
-
-for h in f['hourly'][12:]:
-    sys.stdout.write('{:>4} '.format(h['temperature']))
-sys.stdout.write('\n')
-for h in f['hourly'][12:]:
-    sys.stdout.write('{:>4} '.format(h['human_readable_datetime']))
-sys.stdout.write('\n')
-
-sys.stdout.write('\n{:^60}\n\n'.format('-YOUR DAILY FORECAST-'))
-
-sys.stdout.write('{:>23}'.format('low temperature:'))
-for d in f['daily']:
-    sys.stdout.write('{:>6}'.format(d['temperature_min']))
-sys.stdout.write('\n')
-sys.stdout.write('{:>23}'.format('mean temperature:'))
-for d in f['daily']:
-    sys.stdout.write('{:>6}'.format(d['temperature_mean']))
-sys.stdout.write('\n')
-sys.stdout.write('{:>23}'.format('high temperature:'))
-for d in f['daily']:
-    sys.stdout.write('{:>6}'.format(d['temperature_max']))
-sys.stdout.write('\n')
-sys.stdout.write('{:>23}'.format(''))
-for d in f['daily']:
-    sys.stdout.write('{:>6}'.format(d['human_readable_datetime']))
-sys.stdout.write('\n\n')
+    f = Forecast(datetime.datetime.now()).asdict()
+    if arguments['load_data']:
+        conn = sqlite3.connect('weather.db')
+        c = conn.cursor()
+        '''
+        STATION - always a number. 
+        DATE    - alwys same format. 
+        '''
+        pass
+    elif arguments['get_field']:
+        pass
+    elif arguments['weather']:
+        print_weather(f)
